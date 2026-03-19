476fa403ccea8130c74d12bf80d5ff29ce878d05
Dan Philipp <danphilipp@MacBook-Air-von-Dan.local>
2026-03-19 11:30:50 +1100

    valid_checking methods

--- part_a/search/program.py
+++ part_a/search/program.py
@@ -1,8 +1,10 @@
 # COMP30024 Artificial Intelligence, Semester 1 2026
 # Project Part A: Single Player Cascade
 
+from __future__ import annotations
 from .core import CellState, Coord, Direction, Action, MoveAction, EatAction, CascadeAction, PlayerColor
 from .utils import render_board
+
 
 
 class Node:
@@ -25,8 +27,72 @@ def apply_action(action, node) -> Node:
     pass
 
 def is_valid_move(action, node) -> bool:
-    # All types of moves
-    pass
+    if action.direction == Direction.Down:
+        if action.coord.r >= 7:
+            return False
+        if node.state.get(action.coord + Direction.Down) in node.state:
+            move_to_cellstate = node.state.get(action.coord + Direction.Down)
+            if move_to_cellstate.color == PlayerColor.BLUE:
+                return False
+    elif action.direction == Direction.Up:
+        if action.coord.r <= 0:
+            return False
+        if node.state.get(action.coord + Direction.Up) in node.state:
+            move_to_cellstate = node.state.get(action.coord + Direction.Up)
+            if move_to_cellstate.color == PlayerColor.BLUE:
+                return False
+    elif action.direction == Direction.Left:
+        if action.coord.c <= 0:
+            return False
+        if node.state.get(action.coord + Direction.Left) in node.state:
+            move_to_cellstate = node.state.get(action.coord + Direction.Left)
+            if move_to_cellstate.color == PlayerColor.BLUE:
+                return False
+    if action.direction == Direction.Right:
+        if action.coord.c >= 7:
+            return False
+        if node.state.get(action.coord + Direction.Right) in node.state:
+            move_to_cellstate = node.state.get(action.coord + Direction.Right)
+            if move_to_cellstate.color == PlayerColor.BLUE:
+                return False
+    return True
+
+def is_valid_eat(action, node) -> bool: 
+    if is_valid_move(MoveAction(action.coord, action.direction), node) is False:
+        return False
+    if action.direction == Direction.Down:
+        if node.state.get(action.coord + Direction.Down) not in node.state:
+            return False
+        move_to_cellstate = node.state.get(action.coord + Direction.Down)
+        if move_to_cellstate.color == PlayerColor.BLUE and move_to_cellstate.height > node.state.get(action.coord):
+            return False
+    if action.direction == Direction.Up:
+        if node.state.get(action.coord + Direction.Up) not in node.state:
+            return False
+        move_to_cellstate = node.state.get(action.coord + Direction.Up)
+        if move_to_cellstate.color == PlayerColor.BLUE and move_to_cellstate.height > node.state.get(action.coord):
+            return False
+    if action.direction == Direction.Left:
+        if node.state.get(action.coord.add(Direction.Left)) not in node.state:
+            return False
+        move_to_cellstate = node.state.get(action.coord.add(Direction.Left))
+        if move_to_cellstate.color == PlayerColor.BLUE and move_to_cellstate.height > node.state.get(action.coord):
+            return False
+    if action.direction == Direction.Right:
+        if node.state.get(action.coord.add(Direction.Right)) not in node.state:
+            return False
+        move_to_cellstate = node.state.get(action.coord.add(Direction.Right))
+        if move_to_cellstate.color == PlayerColor.BLUE and move_to_cellstate.height > node.state.get(action.coord):
+            return False
+    return True
+
+def is_valid_cascade(action, node):
+    if node.state.get(action.coord).height >= 2:
+        return True
+    return False
+
+    
+        
 
 def get_path(goal: Node) -> list[Action]:
     pass
@@ -59,6 +125,9 @@ def search(
     # ...
     # ... (your solution goes here!)
     # ...
+    root = create_root(board)
+    print(is_valid_move(MoveAction(Coord(3, 3), Direction.Down), root))
+    print(is_valid_eat(EatAction(Coord(4, 3), Direction.Down), root))
 
     # Here we're returning "hardcoded" actions as an example of the expected
     # output format. Of course, you should instead return the result of your
